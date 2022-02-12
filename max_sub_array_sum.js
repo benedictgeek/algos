2387a5c82f25ef200c373825838073ad812b17ba
Olushola <olushola251@gmail.com>
2022-02-12 16:46:14 +0100

    algosss

---
+++ max_sub_array_sum.js
@@ -0,0 +1,16 @@
+let solution = (array, subArrayLength) => {
+  let sum = 0;
+  //sum first subArray
+  for (let index = 0; index < subArrayLength; index++) {
+    sum = sum + array[index];
+  }
+
+  for (let index = subArrayLength; index < array.length; index++) {
+    let newSum = sum - array[index - subArrayLength] + array[index];
+    sum = Math.max(sum, newSum);
+  }
+
+  return sum;
+};
+
+console.log(solution([1,2,5,2,8,1,5], 4));
---
+++ number_unique_values.js
@@ -0,0 +1,16 @@
+//  count unique values for an ordered array
+
+let countUniqueValues = (array) => {
+  if (array.length == 0) return 0;
+  let i = 0;
+  for (let j = 1; j < array.length; j++) {
+    if (array[i] != array[j]) {
+      i++;
+      array[i] = array[j];
+    }
+  }
+
+  return i + 1;
+};
+
+console.log(countUniqueValues([1, 1, 1, 2, 2, 2, 3, 4, 5, 4, 3]));
